a4689040e5a29ab2674f12fc2494ccdf14419c5d
Enver Onder Uslu <enveronderuslu@gmail.com>
2026-03-12 14:37:39 +0100

    .

--- Infrastructure/VM_Comstruct/ubuntu-virtual.sh
+++ Infrastructure/VM_Comstruct/ubuntu-virtual.sh
@@ -5,17 +5,17 @@ read user
 echo "(host?).example.local"
 read host
 
-SOURCE="/var/lib/libvirt/images/10.07.MGMT-BASTION.qcow2"
+SOURCE="/var/lib/libvirt/images/DOCKER.qcow2"
 DEST="/var/lib/libvirt/images/$user.qcow2"
 
 sudo cp -p "$SOURCE" "$DEST"
 
+sudo qemu-img resize "$DEST" 20G
 
 sudo virt-sysprep -a "$DEST" \
   --hostname "$host.example.local" \
   --root-password password:asd \
   --enable customize,net-hostname,net-hwaddr,machine-id
-
 
 sudo virt-install \
   --name "$user" \
@@ -27,6 +27,3 @@ sudo virt-install \
   --network network=LAB \
   --graphics vnc \
   --noautoconsole
-
-  # 10.06.MGMT-IPA
-  # 10.07.MGMT-BASTION
